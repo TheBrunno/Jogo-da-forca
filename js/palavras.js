5d43b6f964db30de51d9f775fbbf1e3e2b1118b1
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2021-05-16 16:39:05 -0300

    Mudança de cor em cada pagina

--- js/palavras.js
+++ js/palavras.js
@@ -1,5 +1,5 @@
 var palavras = {
-    "açoes": ['Pegar', 'Levantar', 'Sujar', 'Limpar', 'Beijar', 'Procurar', 'Mexer', 'Listar', 'Ler', 'Aprender', 'Rir', 'Chorar', 'Escrever'],
-    "objetos": ['Teclado', 'Mouse', 'Caneta', 'Caderno', 'Tenis', 'Meia', 'Lapis', 'Chinelo', 'Faca', 'Garfo', 'Colher', 'Prato'],
-    "paises": ['Brasil', 'Alemanha', 'Egito', 'França', 'Australia', 'Mexico', 'China', 'Japão', 'Belgica', 'Bolivia', 'Argentina']
+    "açoes": ['Pegar', 'Levantar', 'Sujar', 'Limpar', 'Beijar', 'Procurar', 'Mexer', 'Listar', 'Ler', 'Aprender', 'Rir', 'Chorar', 'Escrever', '#e01e37', '#a11d33'],
+    "objetos": ['Teclado', 'Mouse', 'Caneta', 'Caderno', 'Tenis', 'Meia', 'Lapis', 'Chinelo', 'Faca', 'Garfo', 'Colher', 'Prato', '#468faf', '#01497c'],
+    "paises": ['Brasil', 'Alemanha', 'Egito', 'França', 'Australia', 'Mexico', 'China', 'Japão', 'Belgica', 'Bolivia', 'Argentina', '#ffba08', '#e85d04']
 }
