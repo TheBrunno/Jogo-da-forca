7f0c118bd597006568301d3cd742a699cf331488
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2021-05-15 23:38:12 -0300

    Pagina inicial/ Estilizaçao das paginas de questoes/ Palavras

---
+++ js/palavras.js
@@ -0,0 +1,5 @@
+var palavras = {
+    "açoes": ['Pegar', 'Levantar', 'Sujar', 'Limpar', 'Beijar', 'Procurar', 'Mexer', 'Listar', 'Ler', 'Aprender', 'Rir', 'Chorar', 'Escrever'],
+    "objetos": ['Teclado', 'Mouse', 'Caneta', 'Caderno', 'Tenis', 'Meia', 'Lapis', 'Chinelo', 'Faca', 'Garfo', 'Colher', 'Prato'],
+    "paises": ['Brasil', 'Alemanha', 'Egito', 'França', 'Australia', 'Mexico', 'China', 'Japão', 'Belgica', 'Bolivia', 'Argentina']
+}
