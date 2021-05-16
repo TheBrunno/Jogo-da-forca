7a2c92b65107bfa98238fce241f72d64774c744b
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2021-05-16 19:42:53 -0300

    Sistema de perda

--- js/game.js
+++ js/game.js
@@ -1,13 +1,26 @@
 const botao = document.getElementById('button')
 const space = document.getElementsByClassName('letra')
+const life = document.getElementById('vidas_restantes')
+var vida = 9
+life.innerHTML = vida
 function palpite(space){
+    var possui = false
     var palpite = document.getElementById('palpite').value
     if(palpite.length > 1){
         alert('Apenas a primeira letra será considerada')
     }
     for(let i in palavra){
         if(palavra[i].toUpperCase() == palpite[0].toUpperCase()){
+            var possui = true
             space[i].innerHTML = palavra[i].toUpperCase()
+        }
+    }
+    if(possui == false){
+        vida--
+        life.innerHTML = vida
+        if(vida == 0){
+            alert(`Você perdeu!!\nResposta: ${palavra.toUpperCase()}`)
+            location.reload()
         }
     }
     var palpite = document.getElementById('palpite').value = ''
